bae2be9aeb398a57b3fc344eea842a5d37b8c88a
hydcoder <hyd_coder@163.com>
2019-10-16 11:18:36 +0800

    第一个RestFul接口

--- src/main/java/com/hyd/web/htalker/push/Application.java
+++ src/main/java/com/hyd/web/htalker/push/Application.java
@@ -1,8 +1,25 @@
 package com.hyd.web.htalker.push;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.hyd.web.htalker.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
 /**
  * Description: hTalker
  * Created by Administrator on 2019/10/15 18:00
  */
-public class Application {
+public class Application extends ResourceConfig {
+
+    public Application() {
+        // 注册逻辑处理的包名
+        packages(AccountService.class.getPackage().getName());
+
+        // 注册json转换器
+        register(JacksonJsonProvider.class);
+
+        // 注册日志打印输出
+        register(Logger.class);
+    }
 }
---
+++ src/main/java/com/hyd/web/htalker/push/service/AccountService.java
@@ -0,0 +1,32 @@
+package com.hyd.web.htalker.push.service;
+
+import com.hyd.web.htalker.push.bean.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Description: hTalker
+ * Created by Administrator on 2019/10/16 10:00
+ */
+// 127.0.0.1/api/account/...
+@Path("/account")
+public class AccountService {
+
+    @GET
+    @Path("/login")
+    public String get() {
+        return "you get the login";
+    }
+
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User post() {
+        User user = new User();
+        user.setName("美女");
+        user.setSex(2);
+        return user;
+    }
+}

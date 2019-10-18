ad31086666360818a0e48b9b160ae752e7cbc9d5
hydcoder <hyd_coder@163.com>
2019-10-18 17:54:53 +0800

    增加数据库User和UserFollow模型bean

---
+++ src/main/java/com/hyd/web/htalker/push/bean/db/User.java
@@ -0,0 +1,204 @@
+package com.hyd.web.htalker.push.bean.db;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Description: 用户的model，对应数据库
+ * Created by Administrator on 2019/10/18 10:49
+ */
+@Entity
+@Table(name = "TB_USER")
+public class User {
+
+    // 主键
+    @Id
+    @PrimaryKeyJoinColumn
+    // 主键生成存储的类型为UUID
+    @GeneratedValue(generator = "uuid")
+    // 把生成器的生成规则定义为uuid2,uuid2是常规的toString
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    // 不允许为null， 不允许修改
+    @Column(nullable = false, updatable = false)
+    private String id;
+
+    @Column
+    private String description;
+
+    // 用户名必须唯一
+    @Column(nullable = false, unique = true, length = 128)
+    private String name;
+
+    // 手机必须唯一
+    @Column(nullable = false, unique = true, length = 64)
+    private String phone;
+
+    @Column(nullable = false)
+    private String password;
+
+    // 头像允许为null
+    @Column
+    private String portrait;
+
+    // 性别有初始值，所以不允许为null
+    @Column(nullable = false)
+    private int sex = 0;
+
+    // 通过token可以获取用户信息，所以必须唯一
+    @Column(unique = true)
+    private String token;
+
+    // 用于推送的设备ID
+    @Column
+    private String pushId;
+
+    // 定义为创建时间戳，在创建时就写入数据库
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createAt = LocalDateTime.now();
+
+    // 定义为更新时间戳，在更新时就写入数据库
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updateAt = LocalDateTime.now();
+
+    @Column()
+    private LocalDateTime lastReceivesAt = LocalDateTime.now();
+
+    // 我关注的人列表方法
+    // 对应的数据库表字段为TB_USER_FOLLOW.originId
+    @JoinColumn(name = "originId")
+    // 定义为懒加载，默认加载User信息的时候，并不查询这个集合
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    // 一对多，一个用户可以有很多关注人，每一次关注都是一条记录
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<UserFollow> following = new HashSet<>();
+
+    // 关注我的人的列表方法
+    // 对应的数据库表字段为TB_USER_FOLLOW.targetId
+    @JoinColumn(name = "targetId")
+    // 定义为懒加载，默认加载User信息的时候，并不查询这个集合
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    // 一对多，一个用户可以被很多人关注，每一次关注都是一条记录
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<UserFollow> followers = new HashSet<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPortrait() {
+        return portrait;
+    }
+
+    public void setPortrait(String portrait) {
+        this.portrait = portrait;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getPushId() {
+        return pushId;
+    }
+
+    public void setPushId(String pushId) {
+        this.pushId = pushId;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public LocalDateTime getLastReceivesAt() {
+        return lastReceivesAt;
+    }
+
+    public void setLastReceivesAt(LocalDateTime lastReceivesAt) {
+        this.lastReceivesAt = lastReceivesAt;
+    }
+
+    public Set<UserFollow> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Set<UserFollow> following) {
+        this.following = following;
+    }
+
+    public Set<UserFollow> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(Set<UserFollow> followers) {
+        this.followers = followers;
+    }
+}
